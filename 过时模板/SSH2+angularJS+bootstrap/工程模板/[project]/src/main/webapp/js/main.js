71d6c3d95e66a7da33251738ff6869bf004cb7f4
黑马刘皇叔 <louyingxin@itcast.cn>
2019-01-25 11:10:58 +0800

    ''

--- 过时模板/SSH2+angularJS+bootstrap/工程模板/[project]/src/main/webapp/js/main.js
+++
@@ -1,3 +0,0 @@
-var config = {'headers':{'Content-Type':'application/x-www-form-urlencoded;'}};
-var appService=angular.module('appService',[]);
-var appController=angular.module('appController',['pagination','appService']);
